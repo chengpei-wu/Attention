f4ad50626ddc94198189988d2060c22d7e3a9c00
wcphaha <1683168521@qq.com>
2022-11-12 20:19:44 +0800

    update MultiHeadAttention

--- Attention/self_attention.py
+++ Attention/self_attention.py
@@ -1,7 +1,17 @@
 import torch.nn as nn
 import math
-import matplotlib.pyplot as plt
 import torch.nn.functional
+
+
+class SimpleSelfAttention(nn.Module):
+    def __init__(self, show_att=False):
+        super(SimpleSelfAttention, self).__init__()
+        self.show_att = show_att
+
+    def forward(self, x):
+        atten = torch.nn.functional.softmax(torch.bmm(x, x.permute(0, 2, 1)), dim=-1)
+        output = torch.bmm(atten, x)
+        return (output, atten) if self.show_att else output
 
 
 class SelfAttention(nn.Module):
@@ -23,12 +33,33 @@ class SelfAttention(nn.Module):
         return (output, atten) if self.show_att else output
 
 
-class SimpleSelfAttention(nn.Module):
-    def __init__(self, show_att=False):
-        super(SimpleSelfAttention, self).__init__()
-        self.show_att = show_att
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(self, input_dim, k_dim, v_dim, model_dim, num_heads):
+        super(MultiHeadSelfAttention, self).__init__()
+        self.num_heads = num_heads
+        self.model_dim = model_dim
+        assert model_dim % num_heads == 0
+        self.Q = nn.Linear(input_dim, k_dim)
+        self.K = nn.Linear(input_dim, k_dim)
+        self.V = nn.Linear(input_dim, v_dim)
+        self.W_Q = nn.Linear(k_dim, model_dim)
+        self.W_K = nn.Linear(k_dim, model_dim)
+        self.W_V = nn.Linear(v_dim, model_dim)
+        self.W_O = nn.Linear(model_dim, model_dim)
 
     def forward(self, x):
-        atten = torch.nn.functional.softmax(torch.bmm(x, x.permute(0, 2, 1)), dim=-1)
-        output = torch.bmm(atten, x)
-        return (output, atten) if self.show_att else output
+        q, k, v = self.Q(x), self.K(x), self.V(x)
+
+        # note that, the use of W_Q(W_K, W_V) with shape (k_dim, model_dim) is the same as using multiple independent
+        # weight matrices(nn.Linear()) with shape (k_dim, model_dim // num_heads)
+        multi_q = self.W_Q(q).reshape(-1, x.shape[0], x.shape[1],
+                                      self.model_dim // self.num_heads)
+        multi_k = self.W_K(k).reshape(-1, x.shape[0], x.shape[1],
+                                      self.model_dim // self.num_heads)
+        multi_v = self.W_V(v).reshape(-1, x.shape[0], x.shape[1],
+                                      self.model_dim // self.num_heads)
+
+        heads_atten = torch.nn.functional.softmax(torch.matmul(multi_q, multi_k.permute(0, 1, 3, 2)), dim=-1)
+        concated_heads_atten = torch.matmul(heads_atten, multi_v).reshape(x.shape[0], x.shape[1], -1)
+
+        return self.W_O(concated_heads_atten)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Implementation of variants of attention
 
-## self attention
+## Self Attention
 
 
 
@@ -14,20 +14,43 @@ $$
 
 ```python
 import torch.nn as nn
-import math
 import matplotlib.pyplot as plt
-import torch.nn.functional
 
 
 import Attention.SelfAttention as SelfAttention
 
 x = torch.rand(1, 5, 16)
-att = SelfAttention(16, 32, 32, show_att=True)
-b, atten = att(x)
-print(b.size())
+attention = SelfAttention(16, 64, 64, show_att=True)
+y, atten = attention(x)
+print(y.size())
+# torch.Size([1, 5, 64])
 
 # show the attention of input
 plt.imshow(atten.detach().numpy().squeeze())
 plt.show()
 ```
 
+## Multi Self Attention
+
+### math
+
+$$
+MultiHead(Q,K,V)=Concate(head_1,\dots,head_n)W^O \\
+where\ head_i=Attention(QW^Q_i,KW^K_i,VW^V_i)
+$$
+
+### usage
+
+```python
+import torch.nn as nn
+import torch.nn.functional
+
+
+import Attention.MultiHeadSelfAttention as MultiHeadSelfAttention
+
+x = torch.rand(1, 5, 16)
+attention = MultiHeadSelfAttention(16, 64, 64, 8 * 64, 8)
+y = attention(x)
+print(y.size())
+# torch.Size([1, 5, 512])
+```
